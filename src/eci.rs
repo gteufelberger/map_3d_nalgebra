05fed4ae41166f9ac69e2f13fc13fa475844a6a6
Georg Teufelberger <georg.teufelberger+git@pm.me>
2025-08-01 16:36:55 +0200

    test: Check ECI to ECEF conversion against map_3d library

--- src/eci.rs
+++ src/eci.rs
@@ -9,3 +9,34 @@ where
     let rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), -gst);
     *rotation.matrix() * eci
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use map_3d;
+    use rand::{self, Rng};
+
+    /// Check against `map_3d` libary
+    #[test]
+    fn test_to_ecef_map_3d() {
+        // Set up random number gen
+        let mut rng = rand::rng();
+
+        // Get GST and ECEF coordinates
+        let gst: f64 = rng.random();
+        let x = rng.random();
+        let y = rng.random();
+        let z = rng.random();
+        let ecef = Vector3::new(x, y, z);
+
+        // Convert
+        let result = to_ecef(gst, ecef);
+
+        // Use `map_3d` as refernce
+        let result_map_3d = map_3d::eci2ecef(gst, x, y, z);
+        let result_map_3d = Vector3::new(result_map_3d.0, result_map_3d.1, result_map_3d.2);
+
+        // Compare
+        assert_eq!(result, result_map_3d);
+    }
+}
