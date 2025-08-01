ba1778fa81ef95c4040f77a31ba2637c3797c84c
Georg Teufelberger <georg.teufelberger+git@pm.me>
2025-08-01 16:47:07 +0200

    feat: Add function to convert from ECEF to ECI

---
+++ src/ecef.rs
@@ -0,0 +1,41 @@
+use nalgebra::{Matrix3, Rotation3, Vector3};
+
+/// Convert ECEF coordinates to ECI
+pub fn to_eci<T>(gst: f64, eci: T) -> T
+where
+    Matrix3<f64>: std::ops::Mul<T, Output = T>,
+{
+    let rotation = Rotation3::from_axis_angle(&Vector3::z_axis(), gst);
+    *rotation.matrix() * eci
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use map_3d;
+    use rand::{self, Rng};
+
+    /// Check against `map_3d` libary
+    #[test]
+    fn test_to_eci_map_3d() {
+        // Set up random number gen
+        let mut rng = rand::rng();
+
+        // Get GST and ECEF coordinates
+        let gst: f64 = rng.random();
+        let x = rng.random();
+        let y = rng.random();
+        let z = rng.random();
+        let ecef = Vector3::new(x, y, z);
+
+        // Convert
+        let result = to_eci(gst, ecef);
+
+        // Use `map_3d` as refernce
+        let result_map_3d = map_3d::ecef2eci(gst, x, y, z);
+        let result_map_3d = Vector3::new(result_map_3d.0, result_map_3d.1, result_map_3d.2);
+
+        // Compare
+        assert_eq!(result, result_map_3d);
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,2 @@
+pub mod ecef;
 pub mod eci;
